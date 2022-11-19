eaa6fa37a8e62c533a6a9947ee730cf0123c0bfd
VadimKozhushko <kvad230885@gmail.com>
2022-11-19 18:44:49 +1000

    lesson8 закончен

---
+++ server/cart.js
@@ -0,0 +1,24 @@
+const add = (cart, req) => {
+  cart.contents.push(req.body);
+  return JSON.stringify(cart, null, 4);
+};
+const change = (cart, req) => {
+  let find = cart.contents.find((el) => el.id_product === +req.params.id);
+  find.quantity += req.body.quantity;
+  return JSON.stringify(cart, null, 4);
+};
+
+const remove = (cart, req) => {
+  let index = cart.contents.findIndex((el) => el.id_product === +req.params.id);
+
+  if (index) {
+    cart.contents.splice(index,1);
+  }
+  return JSON.stringify(cart, null, 4);
+};
+
+module.exports = {
+  add,
+  change,
+  remove,
+};
